c8acab8db46ba24c134f3477ba416e257ed67244
Sean Teramae <seteramae@gmail.com>
2018-09-08 15:43:12 -1000

    Add function for reading mashed keys

---
+++ Assets/Scripts/Manager.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System;
+using System.Runtime.CompilerServices;
+using UnityEngine;
+
+ public class Manager : Singleton<Manager> {
+    public string myGlobalVar = "whatever";
+
+ }
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,20 +4,45 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour {
     public float speed;
+    public int successCount;
     private Rigidbody2D rb2d;
-    
+
+    private KeyCode lastPressed = KeyCode.W;
+    private int aCount;
+    private int dCount;
+
     void Start(){
         rb2d = GetComponent<Rigidbody2D>();
+        aCount = dCount = 0;
+        Debug.Log(Manager.Instance.myGlobalVar);
     }
 
-    void FixedUpdate(){
+    void Update(){
         float moveHorizontal = Input.GetAxis("Horizontal");
         float moveVertical = Input.GetAxis("Vertical");
         Vector2 movement = new Vector2(moveHorizontal, moveVertical);
-        rb2d.AddForce(movement * speed);
-        if(Input.GetKey(KeyCode.W)) Debug.Log("W!");
-        else if(Input.GetKey(KeyCode.S)) Debug.Log("S!");
-        else if(Input.GetKey(KeyCode.A)) Debug.Log("A!");
-        else if(Input.GetKey(KeyCode.D)) Debug.Log("D!");
+        rb2d.velocity = movement*speed;
+
+        if(checkWinStatus()){
+            Debug.Log("You Win!");
+        }
     }
+
+    bool checkWinStatus() {
+        if (Input.GetKeyDown(KeyCode.A) && lastPressed != KeyCode.A) {
+            aCount++;
+            Debug.Log("A " + aCount);
+            lastPressed = KeyCode.A;
+        }
+        else if(Input.GetKeyDown(KeyCode.D) && lastPressed != KeyCode.D) {
+            dCount++;
+            Debug.Log("D " + dCount);
+            lastPressed = KeyCode.D;
+        }
+
+        if(aCount + dCount >= successCount){
+            return true;
+        }
+        return false;
+    } 
 }
